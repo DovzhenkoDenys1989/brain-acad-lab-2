69545ec651c9e5ccd0ad82d589f653f408fa1c29
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2020-08-19 10:18:53 +0300

    This is first class

---
+++ src/ua/zp/brainacad/ItemOne.java
@@ -0,0 +1,11 @@
+package ua.zp.brainacad;
+
+public class ItemOne {
+    /**
+     * This is first class
+     */
+    public static void main(String[] args) {
+        System.out.println("This is first app in brainacademy");
+    }
+
+}
---
+++ src/ua/zp/brainacad/ItemTwo.java
@@ -0,0 +1,10 @@
+package ua.zp.brainacad;
+
+public class ItemTwo {
+    /**
+     * THis is second class
+     */
+    public static void main(String[] args) {
+        System.out.println("Довженко Денис");
+    }
+}
