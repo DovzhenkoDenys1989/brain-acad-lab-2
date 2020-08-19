a93ccef888d2622f95ed19684b6a4bd1b3177585
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2020-08-19 10:19:15 +0300

    THis is second class

---
+++ src/ua/zp/brainacad/ItemThree.java
@@ -0,0 +1,14 @@
+package ua.zp.brainacad;
+
+public class ItemThree {
+    /**
+     * This is third class
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("W o r l d");
+    }
+}
+
