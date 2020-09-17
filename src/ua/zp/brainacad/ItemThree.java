383ae46df607054e316a3212160a6996aca43158
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2020-09-17 15:10:31 +0300

    Переделанная лабораторная работа

--- src/ua/zp/brainacad/ItemThree.java
+++ src/ua/zp/brainacad/ItemThree.java
@@ -5,10 +5,12 @@ public class ItemThree {
      * This is third class
      */
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("W o r l d");
+        System.out.println("H");
+        System.out.println("e");
+        System.out.println("l");
+        System.out.println("l");
+        System.out.println("o");
+        System.out.println("W\to\tr\tl\td");
     }
 }
 
